a26ace2043993a623e6d0cb1a61b3f53c9652ddf
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-02-21 16:05:11 +0000

    Update BiasedRandom.cs

--- Assets/LTF_Utils/Biased_Random/BiasedRandom.cs
+++ Assets/LTF_Utils/Biased_Random/BiasedRandom.cs
@@ -7,8 +7,13 @@ namespace LTF.BiasedRandom
     {
         [SerializeField] private float _min;
         [SerializeField] private float _max;
+        [Tooltip("The number which the random number will be biased")]
         [SerializeField] private float _bias;
+        [Tooltip("How strong is the influence. Keep this between 0.0 and 1.0 for better results")]
         [SerializeField] private float _influence;
+
+        public readonly float Min => _min;
+        public readonly float Max => _max;
 
         public BiasedRandom(float min = 0f, float max = 1f, float bias = .5f, float influence = .5f)
         {
